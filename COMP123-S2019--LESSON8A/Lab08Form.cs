84d65d7ab2ceab08f7aa95ee6c00f0c813ad2aeb
301051646 <301051646@PR-E216-57>
2019-06-26 17:26:11 -0400

    Completed initial user interfaces for Lab08

---
+++ COMP123-S2019--LESSON8A/Lab08Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019__LESSON8A
+{
+    public partial class Lab08Form : Form
+    {
+        public Lab08Form()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoGroupBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- COMP123-S2019--LESSON8A/Program.cs
+++ COMP123-S2019--LESSON8A/Program.cs
@@ -16,7 +16,7 @@ namespace COMP123_S2019__LESSON8A
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Lab08Form());
         }
     }
 }
